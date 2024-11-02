29c67c7ad8095e218e616c3fc5223e265824faff
MAWAISTANVEER <mawaistanveer92@gmail.com>
2024-11-02 13:49:40 -0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,42 @@
+number1=10
+number2=25
+number3=35
+#FINDING SUM
+sum=number1+number2+number3
+print("sum:",sum)
+
+#finding difference
+difference=number1-number2-number3
+print("difference:",difference)
+
+#finding product
+product=number1*number2*number3
+print("product:",product)
+
+#finding average
+average=(number1+number2+number3)/3
+print(average,average)
+
+
+#area of triange
+breadth=9
+height=11
+area=0.5*breadth*height
+print("triangle area",area)
+
+#area of square
+side=9
+area=side*side
+print("square area",area)
+
+#area of circle
+pi=8.2
+radius=8
+area=pi*radius*radius
+print("circle area",area)
+
+#area of rectangle
+length=5
+width=10
+area=length*width
+print("rectangle area",area)
